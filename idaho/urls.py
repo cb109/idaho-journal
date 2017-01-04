3d8a58e99a78b2f84e11715f48401614d5893c69
Christoph Buelter <c.buelter@arcor.de>
2017-01-04 21:10:27 +0100

    Remove leftover template stuff.

--- idaho/urls.py
+++ idaho/urls.py
@@ -1,7 +1,5 @@
 from django.conf.urls import url, include
 from django.contrib import admin
-from django.views.generic import TemplateView
-from django.views.generic.base import RedirectView
 
 from idaho.entries import urls as entries_urls
 
@@ -11,7 +9,4 @@ urlpatterns = [
     url(r"^api/", include(entries_urls)),
     url(r"^api-auth/", include("rest_framework.urls",
                                namespace="rest_framework")),
-
-    url(r"^$", RedirectView.as_view(url="publish/")),
-    url(r"^publish/$", TemplateView.as_view(template_name="publish.html")),
 ]
