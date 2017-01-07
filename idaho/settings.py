97f4764b498318076a639fdea1341f1de97cf91c
Christoph Buelter <c.buelter@arcor.de>
2017-01-07 23:07:48 +0100

    Collect other templates like login.html to staticfiles as well.

--- idaho/settings.py
+++ idaho/settings.py
@@ -129,5 +129,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'public', 'static')
 STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'idaho', 'templates'),
     os.path.join(BASE_DIR, 'idaho', 'client', 'dist'),
 ]
