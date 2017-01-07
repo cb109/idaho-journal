dacbb2cc0be23c48fbc74ba4c9b64f8b03e4ed2f
Christoph Buelter <c.buelter@arcor.de>
2017-01-08 00:11:29 +0100

    Remove login urls for now.

--- idaho/urls.py
+++ idaho/urls.py
@@ -1,6 +1,5 @@
 from django.conf.urls import url, include
 from django.contrib import admin
-from django.contrib.auth import views as auth_views
 from django.views.generic.base import TemplateView
 
 from idaho.entries import urls as entries_urls
@@ -8,15 +7,9 @@ from idaho.entries import urls as entries_urls
 
 urlpatterns = [
     url(r"^admin/", admin.site.urls),
-
     url(r"^api/", include(entries_urls)),
     url(r"^api-auth/", include("rest_framework.urls",
                                namespace="rest_framework")),
-
-    url(r"^login/$", auth_views.login,
-        {"template_name": "templates/login.html"}, name="login"),
-    url(r"^logout/$", auth_views.logout, name="logout"),
-
     url(r'^$', TemplateView.as_view(template_name='client.html'),
         name="home"),
 ]
