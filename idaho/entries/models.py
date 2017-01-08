ff617e39c2b8a424c9c66653f6fbd0080ce3bae3
Christoph Buelter <c.buelter@arcor.de>
2017-01-08 02:37:40 +0100

    Add a REST auth token for a User when it is created.

--- idaho/entries/models.py
+++ idaho/entries/models.py
@@ -1,7 +1,23 @@
 from __future__ import unicode_literals
 
-from django.db import models
+import logging
+
 from django.contrib.auth.models import User
+from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from rest_framework.authtoken.models import Token
+
+
+logger = logging.getLogger(__name__)
+
+
+@receiver(post_save, sender=User)
+def create_auth_token(sender, instance, created, **kwargs):
+    """Automatically create a Token for the newly created User."""
+    if created:
+        logger.info("Creating token for user: {}".format(instance.username))
+        Token.objects.create(user=instance)
 
 
 class DiaryEntry(models.Model):
@@ -14,6 +30,6 @@ class DiaryEntry(models.Model):
     body = models.TextField()
 
     def __str__(self):
-        return '{0} from {1}: {2}'.format(self.__class__.__name__,
+        return "{0} from {1}: {2}".format(self.__class__.__name__,
                                           self.author,
                                           self.title)
--- idaho/settings.py
+++ idaho/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     # 3rd party.
     'rest_framework',
+    'rest_framework.authtoken',
     'sslserver',
 
     # Project.
