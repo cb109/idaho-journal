dcfadec0c5047e2001fdff6341393f28729683fb
Christoph Buelter <c.buelter@arcor.de>
2017-01-17 20:55:48 +0100

    Add a schema/ endpoint to display an interactive API documentation.

--- idaho/settings.py
+++ idaho/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     # 3rd party.
     'rest_framework',
     'rest_framework.authtoken',
+    'rest_framework_swagger',
     'sslserver',
 
     # Project.
--- idaho/urls.py
+++ idaho/urls.py
@@ -3,17 +3,21 @@ from django.contrib import admin
 from django.views.generic.base import TemplateView
 
 from rest_framework_jwt.views import obtain_jwt_token, verify_jwt_token
+from rest_framework_swagger.views import get_swagger_view
 
 from idaho.entries import urls as entries_urls
 
 
+schema_view = get_swagger_view(title="idaho API")
+
 urlpatterns = [
     url(r"^admin/", admin.site.urls),
     url(r"^api/", include(entries_urls)),
-    url(r"^api-auth/", include("rest_framework.urls",
+    url(r"^api/schema", schema_view),
+    url(r"^api/auth/", include("rest_framework.urls",
                                namespace="rest_framework")),
-    url(r'^api-token-auth/', obtain_jwt_token),
-    url(r'^api-token-verify/', verify_jwt_token),
+    url(r'^api/token-auth/', obtain_jwt_token),
+    url(r'^api/token-verify/', verify_jwt_token),
     url(r'^$', TemplateView.as_view(template_name='client.html'),
         name="home"),
 ]
