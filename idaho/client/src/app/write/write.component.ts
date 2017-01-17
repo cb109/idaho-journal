b43b11ffe3a7cd5e3c59771ff5b49036e534fe37
Christoph Buelter <c.buelter@arcor.de>
2017-01-17 22:55:41 +0100

    Move writing dummy code to writing component.

--- idaho/client/src/app/write/write.component.ts
+++ idaho/client/src/app/write/write.component.ts
@@ -1,4 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
+
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/throw';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+
+interface TextEntry {
+  author?: number;
+  title: string;
+  body: string;
+}
 
 @Component({
   selector: 'app-write',
@@ -7,9 +19,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WriteComponent implements OnInit {
 
-  constructor() { }
+  entriesUrl = 'http://localhost:8000/api/entries/';
 
-  ngOnInit() {
+  constructor(private http: Http) {}
+
+  ngOnInit() {}
+
+  private createTextEntry(title: string, message: string): TextEntry {
+    var adminId = 1;
+    var entry = {'author': adminId, 'title': title, 'body': message}
+    return entry
+  }
+
+  publishTextEntry(title: string, message: string): void {
+    var entry = this.createTextEntry(title, message);
+    console.log(entry);
+    let headers = new Headers({ 'Content-Type': 'application/json' });
+    let options = new RequestOptions({ headers: headers });
+
+    console.log("sending post!");
+    this.http.post(this.entriesUrl, entry, options)
+             .subscribe();
   }
 
 }
