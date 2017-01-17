f819f00fe241be442e51c6b5130f8378025f62d3
Christoph Buelter <c.buelter@arcor.de>
2017-01-17 21:38:20 +0100

    Add 'Authorization JWT' header to every request automatically, read from 'id_token' localStorage.

--- idaho/client/src/app/app.module.ts
+++ idaho/client/src/app/app.module.ts
@@ -1,12 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { HttpModule, Http, RequestOptions } from '@angular/http';
 
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { provideAuth, AuthHttp, AuthConfig } from 'angular2-jwt';
 
 import { routing, routedComponents } from './app.routing';
 import { AppComponent } from './app.component';
+
+// Workaround for: https://github.com/auth0/angular2-jwt/issues/258
+// TODO: See if we can import and use AUTH_PROVIDERS instead.
+export function authHttpServiceFactory(http: Http, options: RequestOptions) {
+  return new AuthHttp( new AuthConfig({}), http, options);
+}
 
 @NgModule({
   declarations: [
@@ -20,7 +27,13 @@ import { AppComponent } from './app.component';
     NgbModule.forRoot(),
     routing,
   ],
-  providers: [],
+  providers: [
+    {
+      provide: AuthHttp,
+      useFactory: authHttpServiceFactory,
+      deps: [ Http, RequestOptions ]
+    },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
