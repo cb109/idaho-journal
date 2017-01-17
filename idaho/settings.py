90b0b8869289d8c247c4951d9fde1efc88bd63a4
Christoph Buelter <c.buelter@arcor.de>
2017-01-17 20:40:20 +0100

    Add djangorestframework-jwt and enable its JWT authentication.

--- idaho/settings.py
+++ idaho/settings.py
@@ -57,6 +57,16 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+    ),
+}
+
 ROOT_URLCONF = 'idaho.urls'
 
 TEMPLATES = [
--- idaho/urls.py
+++ idaho/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from django.views.generic.base import TemplateView
 
+from rest_framework_jwt.views import obtain_jwt_token, verify_jwt_token
+
 from idaho.entries import urls as entries_urls
 
 
@@ -10,6 +12,8 @@ urlpatterns = [
     url(r"^api/", include(entries_urls)),
     url(r"^api-auth/", include("rest_framework.urls",
                                namespace="rest_framework")),
+    url(r'^api-token-auth/', obtain_jwt_token),
+    url(r'^api-token-verify/', verify_jwt_token),
     url(r'^$', TemplateView.as_view(template_name='client.html'),
         name="home"),
 ]
