df13c5a97dee50add20c2e9a1baf3604a2b5cca2
Christoph Buelter <c.buelter@arcor.de>
2017-01-21 23:06:39 +0100

    Add a note about currently disabled model permissions.

--- idaho/entries/views.py
+++ idaho/entries/views.py
@@ -7,4 +7,6 @@ from idaho.entries.serializers import DiaryEntrySerializer
 class DiaryEntryViewSet(viewsets.ModelViewSet):
     queryset = DiaryEntry.objects.all()
     serializer_class = DiaryEntrySerializer
+    # FIXME: Reenable this once all our frontend requests use the JWT
+    #   in their request headers.
     # permission_classes = (permissions.IsAuthenticated,)
