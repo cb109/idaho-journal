7ffd2ff3027903a950f3766f3b8bb925b5b8172c
Christoph Buelter <c.buelter@arcor.de>
2017-01-21 18:59:47 +0100

    Add FIXME note about initial auth-guard bug.

--- idaho/client/src/app/auth-guard.service.ts
+++ idaho/client/src/app/auth-guard.service.ts
@@ -10,6 +10,10 @@ export class AuthGuardService implements CanActivate {
   constructor(private authService: AuthService, 
               private router: Router) {}
 
+  // FIXME: This is buggy as when reloading the page the initial
+  //   value of 'isLoggedIn' is always false, but may change when
+  //   the promise resolves. We need to wait for it and work with
+  //   Observables here, probably also in the auth.service.ts.
   canActivate() {
     if (!this.authService.isLoggedIn) {
       console.log('You must be logged in to view this. Redirecting...');
