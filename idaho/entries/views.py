40d8f96ba9a8ea819e29981d646a3aa987bd6705
Christoph Buelter <c.buelter@arcor.de>
2017-01-30 23:50:42 +0100

    Return only entries from requesting user.

--- idaho/entries/views.py
+++ idaho/entries/views.py
@@ -5,8 +5,9 @@ from idaho.entries.serializers import DiaryEntrySerializer
 
 
 class DiaryEntryViewSet(viewsets.ModelViewSet):
-    queryset = DiaryEntry.objects.all()
     serializer_class = DiaryEntrySerializer
-    # FIXME: Reenable this once all our frontend requests use the JWT
-    #   in their request headers.
     permission_classes = (permissions.IsAuthenticated,)
+
+    def get_queryset(self):
+        """Only return entries written by the requesting user."""
+        return DiaryEntry.objects.filter(author=self.request.user)
