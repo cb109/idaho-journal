55cdbac61140f0fa88b961066243a1c4754253c7
Christoph Buelter <c.buelter@arcor.de>
2017-02-04 01:44:00 +0100

    Implement deletion of entries and only display those that are not.

--- idaho/client/src/app/read/read.component.ts
+++ idaho/client/src/app/read/read.component.ts
@@ -1,5 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { Headers, RequestOptions } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/of';
+import 'rxjs/add/operator/catch';
 
+import { ToastrService } from 'toastr-ng2';
+import { AuthHttp } from 'angular2-jwt';
+
+import { environment } from '../../environments/environment';
 import { Entry } from '../entry';
 import { EntriesService } from '../entries.service';
 import { Encrypted, EncryptionService } from '../encryption.service';
@@ -15,11 +23,17 @@ export class ReadComponent implements OnInit {
   private _encryptedEntries: Entry[] = [];
   decryptedEntries: Entry[] = [];
 
-  constructor(private entriesService: EntriesService,
+  constructor(private authHttp: AuthHttp,
+              private entriesService: EntriesService,
               private passwordService: PasswordService,
-              private encryptionService: EncryptionService) { }
+              private encryptionService: EncryptionService,
+              private toastr: ToastrService) { }
 
   ngOnInit() {
+    this.decryptEntries();
+  }
+
+  decryptEntries() {
     this.entriesService.getEntries()
       .then(entries => {
         this._encryptedEntries = entries.slice().reverse();
@@ -36,6 +50,31 @@ export class ReadComponent implements OnInit {
           entry.body = this.encryptionService.fromEncryptedString(password,
                                                                   entry.body);
           this.decryptedEntries.push(entry);
+        }
+      });
+  }
+  deleteEntry(entry: Entry): void {
+    var deleteEntryUrl = environment.entriesUrl + entry.id + '/';
+    var headers = new Headers({});
+    var options = new RequestOptions({ headers: headers });
+    this.authHttp
+      .delete(deleteEntryUrl, options)
+      .catch(error => {
+        console.error(error);
+        this.toastr.error(
+          'Your entry could not be deleted: ' + error._body,
+          'Removal failed');
+        return Observable.of(error);
+      })
+      .subscribe(response => {
+        // Update frontend as well.
+        var idx = this.decryptedEntries.indexOf(entry);
+        this.decryptedEntries.splice(idx, 1);
+
+        if (response.ok) {
+          this.toastr.success(
+            '"' + entry.title + '" has been deleted.',
+            'Removal successful');
         }
       });
   }
