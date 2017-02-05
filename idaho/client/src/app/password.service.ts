693dc12b44644047cfb86a4d76da7bbf5f2903b0
Christoph Buelter <c.buelter@arcor.de>
2017-02-05 20:34:20 +0100

    Add unicode support to passwords.

--- idaho/client/src/app/password.service.ts
+++ idaho/client/src/app/password.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 
+declare var escape, unescape: any;  // Browser helper functions.
+
 @Injectable()
 export class PasswordService {
 
@@ -7,8 +9,22 @@ export class PasswordService {
 
   constructor() { }
 
+  // Helper functions to support unicode in encrypted data.
+  // https://github.com/bitwiseshiftleft/sjcl/issues/53
+  // https://developer.mozilla.org/en-US/docs/Web/API/WindowOrWorkerGlobalScope/btoa
+
+  // ucs-2 string to base64 encoded ascii
+  utoa(str: string) {
+    return window.btoa(unescape(encodeURIComponent(str)));
+  }
+
+  // base64 encoded ascii to ucs-2 string
+  atou(str: string) {
+    return decodeURIComponent(escape(window.atob(str)));
+  }
+
   store(password: string): string {
-    var base64Password = btoa(password);
+    var base64Password = this.utoa(password);
     sessionStorage.setItem(this.passwordName, base64Password);
     return base64Password;
   }
@@ -18,7 +34,7 @@ export class PasswordService {
     if (!base64Password) {
       return '';
     }
-    var password = atob(base64Password);
+    var password = this.atou(base64Password);
     return password;
   }
 
