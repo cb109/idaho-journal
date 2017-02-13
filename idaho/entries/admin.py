eeec01b623a2c8348c1ec5db3f71cf12a66722ac
Christoph Buelter <info@cbuelter.de>
2017-02-13 09:16:51 +0100

    Make 'delete' attr editable in admin view.

--- idaho/entries/admin.py
+++ idaho/entries/admin.py
@@ -19,19 +19,22 @@ class DiaryEntryAdmin(admin.ModelAdmin):
         "deleted",
     )
     list_display = (
-        "content",
-        "author",
-        "kind",
-        "created_at",
-        "modified_at",
         "id",
+        "created_at",
+        "kind",
+        "author",
+        "deleted",
+        "modified_at",
+        "content",
+    )
+    list_editable = (
         "deleted",
     )
 
     def content(self, obj):
         """Limit maximum length of displayed content."""
         title_obj = json.loads(json.loads(obj.title))
-        return title_obj["ct"][:50]
+        return title_obj["ct"][:20]
 
 admin.site.site_header = "idaho Administration"
 admin.site.register(DiaryEntry, DiaryEntryAdmin)
