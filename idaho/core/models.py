bee77c48ff0de10c4aa27aa0a7f1059a5096c422
Christoph Buelter <c.buelter@arcor.de>
2017-03-01 00:44:06 +0100

    Add core app with a simple UserProfile model.

---
+++ idaho/core/models.py
@@ -0,0 +1,21 @@
+from django.contrib.auth.models import User
+from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+
+class UserProfile(models.Model):
+    """Store additional data for a User."""
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    email_reminder_enabled = models.BooleanField(default=False)
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
