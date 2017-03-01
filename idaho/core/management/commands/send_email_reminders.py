731ea7bec4f392037e12a69cc8cf798330fc9f3d
Christoph Buelter <c.buelter@arcor.de>
2017-03-01 18:30:22 +0100

    Allow html in the email reminder.

--- idaho/core/management/commands/send_email_reminders.py
+++ idaho/core/management/commands/send_email_reminders.py
@@ -10,12 +10,14 @@ to be configured in the Django settings. This is a simple solution so
 that running our own SMTP server is not required, but remember that
 Google limits the mail API usage (we can send ~ 500 mails a day).
 
-We don't store here how often this command is run, so it is up to you to
+We don"t store here how often this command is run, so it is up to you to
 run it once per day or whatever. Simplest solution would be to add this
 as a daily cron job.
 
 """
 import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 from django.conf import settings
 from django.core.management.base import BaseCommand
@@ -32,12 +34,33 @@ def send_email_via_gmail(gmail_user, gmail_pwd, recipient, subject, body):
         recipient (str|list[str]): One or more email addresses to send to.
         subject (str): Subject for the email to use.
         body (str): Message body of the email.
+
+    The email will contain a plain/text and an html version.
     """
     from_email = gmail_user
     to_emails = recipient if type(recipient) is list else [recipient]
 
-    message = ("From: {0}\nTo: {1}\nSubject: {2}\n\n{3}"
-               .format(from_email, ", ".join(to_emails), subject, body))
+    msg = MIMEMultipart("alternative")
+    msg["Subject"] = subject
+    msg["From"] = from_email
+    msg["To"] = ", ".join(to_emails)
+
+    html = """
+    <html>
+      <body style="font-family: sans-serif; margin: 20px">
+        <h1>{header}</h1>
+        {text}
+      </body>.
+    </html>
+    """.format(header=subject, text=body)
+
+    # Attach parts into message container.
+    # According to RFC 2046, the last part of a multipart message, in
+    # this case the HTML message, is best and preferred.
+    part1 = MIMEText(body, "plain")
+    part2 = MIMEText(html, "html")
+    msg.attach(part1)
+    msg.attach(part2)
 
     server = smtplib.SMTP(settings.EMAIL_REMINDER["GMAIL_SMTP_SERVER"],
                           settings.EMAIL_REMINDER["GMAIL_SMTP_PORT"])
@@ -45,7 +68,7 @@ def send_email_via_gmail(gmail_user, gmail_pwd, recipient, subject, body):
         server.ehlo()
         server.starttls()
         server.login(gmail_user, gmail_pwd)
-        server.sendmail(from_email, to_emails, message)
+        server.sendmail(from_email, to_emails, msg.as_string())
     finally:
         server.quit()
 
