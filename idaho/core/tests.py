b382249532b0ebe54182e7d549802650c82793e3
Christoph Buelter <c.buelter@arcor.de>
2017-03-02 23:26:10 +0100

    Add tests for the UserProfile.

--- idaho/core/tests.py
+++ idaho/core/tests.py
@@ -1,3 +1,14 @@
-from django.test import TestCase
+import pytest
 
-# Create your tests here.
+from idaho.core.models import UserProfile
+
+
+def test_userprofile_is_created_automatically_for_new_user(user):
+    assert isinstance(user.userprofile, UserProfile)
+
+
+def test_userprofile_is_deleted_with_its_user(user):
+    userprofile_id = user.userprofile.id
+    user.delete()
+    with pytest.raises(UserProfile.DoesNotExist):
+        UserProfile.objects.get(id=userprofile_id)
