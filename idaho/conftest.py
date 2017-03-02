a564fa9eb3ec7e20f302a98ea3efa643bda78e87
Christoph Buelter <c.buelter@arcor.de>
2017-03-03 00:32:21 +0100

    Specify a password for the user fixture.

--- idaho/conftest.py
+++ idaho/conftest.py
@@ -1,12 +1,25 @@
 """Shared test fixtures."""
 
 import pytest
+from freezegun import freeze_time
 from django.contrib.auth.models import User
+
+from idaho.entries.models import DiaryEntry
 
 
 @pytest.fixture
 def user(db):
     """Create a new User in the database and return an instance."""
-    user = User(username="test-user")
-    user.save()
+    user = User.objects.create_user(username="user",
+                                    password="password")
     return user
+
+
+@freeze_time("2011-11-1")
+@pytest.fixture
+def entry(user):
+    """Create a DiaryEntry of kind text with frozen timestamps."""
+    entry = DiaryEntry(kind="text", author=user, title="My Title",
+                       body="My Body")
+    entry.save()
+    return entry
