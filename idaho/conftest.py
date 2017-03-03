7e7dbb447ce945d7ca2a2b1f01e8ae6ee90b3141
Christoph Buelter <c.buelter@arcor.de>
2017-03-03 13:35:59 +0100

    Add test for /entries/count/.

--- idaho/conftest.py
+++ idaho/conftest.py
@@ -15,6 +15,14 @@ def user(db):
     return user
 
 
+@pytest.fixture
+def another_user(db):
+    """Create a new User in the database and return an instance."""
+    user = User.objects.create_user(username="another_user",
+                                    password="another_password")
+    return user
+
+
 @freeze_time("2011-11-1")
 @pytest.fixture
 def entry(user):
@@ -23,3 +31,25 @@ def entry(user):
                        body="My Body")
     entry.save()
     return entry
+
+
+@pytest.fixture
+def entries(user, another_user):
+    """Create a few different DiaryEntry with frozen timestamps."""
+    entries = [
+        DiaryEntry(kind="text", author=user, title="My Text Entry",
+                   body="My Text Body"),
+        DiaryEntry(kind="image", author=user, title="My Image Entry",
+                   body="My Image Body"),
+        DiaryEntry(kind="audio", author=user, title="My Audio Entry",
+                   body="My Audio Body"),
+        DiaryEntry(kind="text", author=another_user,
+                   title="My other Text Entry", body="My other Text Body"),
+        DiaryEntry(kind="image", author=another_user,
+                   title="My other Image Entry", body="My other Image Body"),
+        DiaryEntry(kind="audio", author=another_user,
+                   title="My other Audio Entry", body="My other Audio Body"),
+    ]
+    for entry in entries:
+        entry.save()
+    return entries
---
+++ idaho/entries/tests.py
@@ -0,0 +1,44 @@
+import pytest
+import requests
+from rest_framework import status
+
+from idaho.entries.models import DiaryEntry
+
+
+def test_diaryentry_has_automatic_datetimefields(entry):
+    """Note: We use freezegun in the fixture to have static datetimes."""
+    expected_datetime_str = "2011-11-01 00:00:00+00:00"
+    assert str(entry.created_at) == expected_datetime_str
+    assert str(entry.modified_at) == expected_datetime_str
+
+
+def test_soft_deletion(entry):
+    """Check that the DiaryEntry is not actually removed."""
+    entry_id = entry.id
+    assert not entry.deleted
+
+    entry.delete()
+
+    entry = DiaryEntry.objects.get(id=entry_id)
+    assert entry
+    assert entry.deleted
+
+
+@pytest.fixture(scope="class")
+def entries_url(live_server):
+    return live_server.url + "/api/entries/"
+
+
+@pytest.fixture
+def user_auth(user):
+    return ("user", "password")
+
+
+def test_entries_count(live_server, entries):
+    url = live_server.url + "/api/entries/count/"
+    basic_auth = ("user", "password")
+
+    response = requests.get(url, auth=basic_auth)
+    assert response.status_code == status.HTTP_200_OK
+    # Only half of the entries are from this user.
+    assert response.json() == 3
